2b207875f57dce2306072204253233d4b51be5c1
vemi <ve@vemic.net>
2024-09-29 19:42:34 +0900

    refactor: Replace ObjectMapper with JsonMapper in JsoncMapper for improved JSON handling

--- lib/src/main/java/jp/vemi/jsoncmapper/JsoncMapper.java
+++ lib/src/main/java/jp/vemi/jsoncmapper/JsoncMapper.java
@@ -2,9 +2,9 @@ package jp.vemi.jsoncmapper;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 
-public class JsoncMapper extends ObjectMapper {
+public class JsoncMapper extends JsonMapper {
     @Override
     public <T> T readValue(String content, Class<T> valueType) throws JsonProcessingException, JsonMappingException {
         String json = JsoncUtils.removeComments(content);
